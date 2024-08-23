727b9a6a96d1c84c02ded4d8432cdf08366be3f9
ChristopherWMM <christopherwmm@gmail.com>
2024-08-22 23:16:43 -0700

    Fix path issue?

--- slides/test/components/Counter.vue
+++ slides/test/components/Counter.vue
@@ -18,7 +18,7 @@ const counter = ref(props.count)
       font="mono"
       outline="!none"
       hover:bg="gray-400 opacity-20"
-      @click="counter -= 23"
+      @click="counter -= 24"
     >
       -
     </button>
